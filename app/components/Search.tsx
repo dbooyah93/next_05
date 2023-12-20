fe993a6949be1c914f1a6c142cad162ab2e6d3d5
David C <51928503+dbooyah93@users.noreply.github.com>
2023-12-20 15:14:45 -0600

    Creates form for search component including router

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,20 +1,31 @@
 'use client'
 
-import { useState } from "react"
+import { useState, FormEvent } from "react"
 import { useRouter } from "next/router"
 
 export default function Search () {
     const [search, setSearch] = useState('')
     const router = useRouter()
-    const handleSubmit = async (event) => {
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         setSearch('')
         router.push(`/${search}/`)
+        // pushes rout onto page address
     }
 
     return (
-        <div>
-            Search
-        </div>
+        <form
+            className="w-50 justify-center md:justify-between"
+            onSubmit={handleSubmit}>
+            <input type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="bg-white p-2 w-80 text-xl rounded-xl"
+                placeholder="Search"
+            />
+            <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
+                🚀
+            </button>
+        </form>
     )
 }
