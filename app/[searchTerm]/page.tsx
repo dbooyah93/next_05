286fdc189c84c034c4044faa46d295132833c921
David C <51928503+dbooyah93@users.noreply.github.com>
2023-12-27 20:27:11 -0600

    Strucutre or search results page including tailwind

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -7,33 +7,24 @@ type Props = {
     }
 }
 
-export default function SearchResults ({ params: { searchTerm } }: Props) {
-    return (
-        <h1>Search page</h1>
+export default async function SearchResults ({ params: { searchTerm } }: Props) {
+    const wikiData: Promise<SearchResult> = getWikiResults( searchTerm )
+    const data = await wikiData
+    const results: Result[] | undefined = data?.query?.pages
+
+    const content = (
+        <div className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+            <h2 className='p-2 text-xl'>Results:</h2>
+            {results
+            // Object.map returns an array of the result generated on each loop
+                    ? Object.values( results ).map( (result, i) => {
+                        return <p key={i}>
+                            { JSON.stringify(result) }
+                        </p>
+                    })
+                    : <h2 className="p-2 text-white text-xl">{`${searchTerm} Not Found`}</h2>
+                    }
+        </div>
     )
+    return content
 }
-
-// export default async function SearchResults ({ params: { searchTerm } }: Props) {
-//     console.log('ching')
-//     const wikiData: Promise<SearchResult> = getWikiResults( searchTerm )
-//     const data = await wikiData
-//     const results: Result[] | undefined = data?.query?.pages
-
-//     const content = (
-//         <div className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
-//             <h2> results</h2>
-//             {results
-//             // Object.map returns an array of the result generated on each loop
-//                     ? Object.values( results ).map( (result, i) => {
-//                         return <p key={i}>
-//                             { JSON.stringify(result) }
-//                         </p>
-//                     })
-//                     : <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
-//                     }
-//         </div>
-//     )
-//     return (
-//               <div>page</div>
-//           )
-// }
